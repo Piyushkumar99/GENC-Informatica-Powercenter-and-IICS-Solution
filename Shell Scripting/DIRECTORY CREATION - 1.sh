72917e9164594420c47d399c13add4c8a4b71aaa
Piyush_Code99 <73519381+Piyushkumar99@users.noreply.github.com>
2023-04-01 17:26:29 +0530

    Add files via upload

---
+++ Shell Scripting/DIRECTORY CREATION - 1.sh
@@ -0,0 +1,11 @@
+DIRECTORY CREATION - 1
+
+
+Create the directory structure given in the image using mkdir for directory creation and touch for creating files.
+
+Note:assume that you are inside your home directory and always specify only relative path.
+
+
+
+mkdir -p mydir/{colors/{basic,blended},shape,animals/{mammals,reptiles}}
+touch mydir/{colors/{basic/{red,blue,green},blended/{yellow,orange,pink}},shape/{circle,square,cube},animals/{platypus,bat,dog},reptiles/{snakes,crocodile,lizard}}}
